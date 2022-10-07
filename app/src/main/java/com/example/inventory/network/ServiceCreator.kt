a122a17e940146f3368b2301395ef797ea71a122
wuhongru <1425636954@qq.com>
2022-10-07 11:21:03 +0800

    +更正网络

--- app/src/main/java/com/example/inventory/network/ServiceCreator.kt
+++ app/src/main/java/com/example/inventory/network/ServiceCreator.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceCreator {
-//    private const val BASE_URL = "http://110.40.156.9:9090/"
-    private const val BASE_URL = "http://192.168.3.86:9090/"
+    private const val BASE_URL = "http://110.40.156.9:9090/"
+//    private const val BASE_URL = "http://192.168.3.86:9090/"
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
